bb77e3b8b77f1e40ea25b6b04d1316c5b9a24beb
Ahmed Iqbal Bhatti <ahmad786687iqbal@gmail.com>
2022-11-09 11:10:28 +0500

    Model testing

--- store/admin.py
+++ store/admin.py
@@ -1,3 +1,21 @@
+# django imports
 from django.contrib import admin
 
+# models imports
+from .models import Category, Product
+
+
 # Register your models here.
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ['name', 'slug']
+    prepopulated_fields = {'slug': ('name',)}
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['title', 'author', 'slug', 'price', 'in_stock', 'created', 'updated']
+    list_filter = ['in_stock', 'is_active', 'author']
+    list_editable = ['price', 'in_stock']
+    prepopulated_fields = {'slug': ('title',)}
--- store/tests.py
+++ store/tests.py
@@ -1,3 +1,39 @@
+from django.contrib.auth.models import User
 from django.test import TestCase
+from store.models import Category, Product
+
 
 # Create your tests here.
+
+class TestCategoriesModel(TestCase):
+
+    def setUp(self):
+        self.data1 = Category.objects.create(name='Graphics', slug='graphics')
+
+    def test_category_model_entry(self):
+        """
+        Test Category Model Data insertion/Types/Field attributes
+        """
+
+        data = self.data1
+        self.assert_(isinstance(data, Category))
+        self.assertEqual(str(data), 'Graphics')
+
+
+class TestProductsModel(TestCase):
+    def setUp(self):
+        self.data1 = Category.objects.create(name='Graphics', slug='graphics')
+        User.objects.create(username='admin')
+        self.data1 = Product.objects.create(category_id=1, title='graphics beginners',
+                                            created_by_id=1, slug='graphics-beginners',
+                                            price='20.00', image='graphics')
+
+    def test_products_model_entry(self):
+        """
+        Test Product model data insetion/types/filed attributes
+        :return:
+        """
+
+        data = self.data1
+        self.assertTrue(isinstance(data, Product))
+        self.assertEqual(str(data), 'graphics beginners')
